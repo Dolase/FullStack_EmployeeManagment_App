404d5cb767a0e90b8277f49b321474b73bfe44b2
Dadasaheb Dolase <136950991+Dolase@users.noreply.github.com>
2024-02-15 22:54:53 +0530

    Add files via upload

---
+++ Full-stack-ems/Frontend_ems/ems-app-frontend/src/Component/FooterComp.js
@@ -0,0 +1,10 @@
+export const FootComp = () =>{
+    return(
+        <div>
+            <footer className="footer">
+                <span><b>All  Rights  Reserved  By  Dadasaheb </b><br></br>
+                  <b>dolasedada28@gmail.com</b></span>
+            </footer>
+        </div>
+    )
+}
